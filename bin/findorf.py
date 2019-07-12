5c475e7728204f254fdfd003443804f604baa436
shjenkins94 <52809855+shjenkins94@users.noreply.github.com>
2019-07-12 17:12:10 +0900

    Update findorf.py
    Added commentary, moved flank and added min_protein_length to the top of findorf

--- bin/findorf.py
+++ bin/findorf.py
@@ -3,6 +3,14 @@ import argparse
 from Bio import SeqIO
 
 
+# 6. orffinder goes through seq 6 times to get all 6 possible reading frames
+#    Each time the potential reading frame is translated
+#    A while loop goes through each amino acid in the reading frame.
+#    If an AA is methionine then the next stop codon "*" is located
+#    If the resulting fragment is longer than min protein length, then the 
+#    start, stop and strand are added to orfs
+#    After possible sequences from all 6 reading frames are added, orfs is 
+#    sorted and returned.
 def orffinder(seq, min_protein_length):
     orfs = []
     seq_len = len(seq)
@@ -37,7 +45,8 @@ def orffinder(seq, min_protein_length):
     orfs.sort()
     return orfs
 
-
+# 4. Extends seq upstream and downstream flank bp or to the start/end of the reference 
+#    sequence, whichever is shortest
 def getsubseq(seq, start, end, flank):
     seq_len = len(seq)
     seq_start = max(0, start - flank)
@@ -45,16 +54,22 @@ def getsubseq(seq, start, end, flank):
     return seq_start, seq_end, seq[seq_start - 1:seq_end]
 
 
+# 2. Defines flank and min_protein_length and converts the reference sequence to a dict
+# 3. For each hit in the best-hit file, extracts the corresponding reference sequence
+#    and sends it, flank and the start and end positions of the hit to getsubseq  
+# 5. Send extended sequence and min_protein_length to orffinder
+# 6. Each potential reading frame is added to orf, which is then printed
 def findorf(args):
+    flank = 1000
+    min_protein_length = 250
     ref_dict = SeqIO.to_dict(SeqIO.parse(args.reference, "fasta"))
     for line in args.hit:
         hit = line.rstrip().split()
         chrom = hit[0]
         ref_record = ref_dict[chrom]
-        flank = 1000
         ref_start, ref_stop, seq = getsubseq(
             ref_record.seq, int(hit[3]), int(hit[4]), flank)
-        orfs = orffinder(seq, 250)
+        orfs = orffinder(seq, min_protein_length)
         for orf_start, orf_stop, strand in orfs:
             orf = [ref_record.id, "findorf", "ORF",
                    str(ref_start + (orf_start - 1)),
@@ -63,6 +78,7 @@ def findorf(args):
             print('\t'.join(orf))
 
 
+# 1. Defines required arguments and calls findorf
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("find open reading frame")
     parser.add_argument(
