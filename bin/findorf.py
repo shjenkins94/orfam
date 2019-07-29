cfaf739cbf67d49c9a5a7638d9bd6c41a7d7fb7c
shjenkins94 <52809855+shjenkins94@users.noreply.github.com>
2019-07-29 12:00:09 +0900

    Revert "Update findorf.py"
    This reverts commit b8a69fba4d7bd37945a68433ff4c6ef128051a21.

--- bin/findorf.py
+++ bin/findorf.py
@@ -3,7 +3,6 @@ import argparse
 from Bio import SeqIO
 
 
-<<<<<<< Updated upstream
 # 3. If seq is on the negative strand, the reverse complement of seq is
 #    assigned to nuc. orffinder then goes through nuc 3 times to get all
 #    reading frames on strand.
@@ -16,17 +15,6 @@ from Bio import SeqIO
 #    There are no unkown proteins ("X") in the sequence
 #    the start and stop positions on the original sequence are recorded
 def orffinder(seq, min_protein_length, strand):
-=======
-# 6. orffinder goes through seq 6 times to get all 6 possible reading frames
-#    Each time the potential reading frame is translated
-#    A while loop goes through each amino acid in the reading frame.
-#    If an AA is methionine then the next stop codon "*" is located
-#    If the resulting fragment is longer than min protein length, then the
-#    start, stop and strand are added to orfs
-#    After possible sequences from all 6 reading frames are added, orfs is
-#    sorted and returned.
-def orffinder(seq, min_protein_length):
->>>>>>> Stashed changes
     orfs = []
     seq_len = len(seq)
     if strand == "-":
@@ -70,7 +58,6 @@ def orffinder(seq, min_protein_length):
     orfs.sort()
     return orfs
 
-<<<<<<< Updated upstream
 
 # 2. Defines  min_protein_length and converts the subject genome
 #    to a dictionary
@@ -78,25 +65,6 @@ def orffinder(seq, min_protein_length):
 #    from the subject genome and sends it and min_protein lenghth to orffinder
 # 4. Each potential reading frame is added to orf, which is then printed as a
 #    BED file
-=======
-
-# 4. Extends seq upstream and downstream flank bp or to the start/end of the
-#    reference sequence, whichever is shortest
-def getsubseq(seq, start, end, flank):
-    seq_len = len(seq)
-    seq_start = max(0, start - flank)
-    seq_end = min(end + flank, seq_len)
-    return seq_start, seq_end, seq[seq_start - 1:seq_end]
-
-
-# 2. Defines flank and min_protein_length and converts the reference sequence
-#    to a dict
-# 3. For each hit in the best-hit file, extracts the corresponding reference
-#    sequence and sends it, flank and the start and end positions of the hit to
-#    getsubseq
-# 5. Send extended sequence and min_protein_length to orffinder
-# 6. Each potential reading frame is added to orf, which is then printed
->>>>>>> Stashed changes
 def findorf(args):
     min_protein_length = args.min_p_len
     with open(args.subject, "rU") as in_subject:
