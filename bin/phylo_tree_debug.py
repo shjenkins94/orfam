173d74a599dcf19c1dc4e100c45c0ef6e21053a7
shjenkins94 <52809855+shjenkins94@users.noreply.github.com>
2019-08-06 15:21:25 +0900

    Update phylo_tree_debug.py
    Used OUT_ID

--- bin/phylo_tree_debug.py
+++ bin/phylo_tree_debug.py
@@ -16,18 +16,21 @@ lab 518, Conservation Biology on Endangered Wildlife, SiChuan University
 """ % __version__
 
 
+# 2. Convert nwk to a tree and outgroup_id to a list.
 def phylo_tree(args):
     with open(args.nwk, "rU") as in_nwk:
         tree = Phylo.read(in_nwk, "newick")
-    outgroup = []
-    orgroup = []
-
     with open(args.outgroup_id, "rU") as in_outgroup:
         non_or = in_outgroup.read().splitlines()
 
+    outgroup = []
+    orgroup = []
+
+# 3. Add clades that contain outgroups to outgroup.
     for non_or_id in non_or:
         outgroup.append(tree.find_clades(non_or_id))
 
+# 4. Potential ors that aren't in the outgroup clades are added to orgroup.
     terminals = tree.get_terminals()
     for terminal in terminals:
         if terminal not in outgroup:
@@ -35,6 +38,7 @@ def phylo_tree(args):
 
     # If genes with that in the outgroup share one MRCA, it may be a non-OR
     # gene and will be discarded.
+# 5. If the outgroup clades are not monophyletic
     if not tree.is_monophyletic(outgroup):
         mrca = tree.common_ancestor(non_or)
         terminals = mrca.get_terminals()
@@ -43,6 +47,7 @@ def phylo_tree(args):
                 print(terminal.name)
 
 
+# 1. Take in arguments nwk and outgroup_id, and send them to phylo_tree
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=INFO)
     parser.add_argument("nwk",
