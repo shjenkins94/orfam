a7b182ddfa88c15045883eae483b38fc4738a79a
shjenkins94 <52809855+shjenkins94@users.noreply.github.com>
2019-08-18 21:02:45 +0900

    Update fastatools.py
    Remove type = argparse.Filetype, since it doesn't close files.

--- bin/fastatools.py
+++ bin/fastatools.py
@@ -6,34 +6,34 @@ from Bio import SeqIO
 
 
 def _translate(args):
-    for record in SeqIO.parse(args.fasta, 'fasta'):
-        print(">" + record.id)
-        print(record.seq.translate())
+    with open(args.fasta, "r") as in_fasta:
+        for record in SeqIO.parse(in_fasta, 'fasta'):
+            print(">" + record.id)
+            print(record.seq.translate())
 
 
 def _extract(args):
-    extract_ids = []
-    for line in args.ids:
-        if len(line.strip()) > 0:
-            extract_ids.append(line.strip())
-    for record in SeqIO.parse(args.fasta, 'fasta'):
-        if record.id in extract_ids:
-            print(record.format('fasta'))
+    with open(args.ids, "r") as in_ids:
+        extract_ids = in_ids.read().splitlines()
+    with open(args.fasta, "r") as in_fasta:
+        for record in SeqIO.parse(in_fasta, 'fasta'):
+            if record.id in extract_ids:
+                print(record.format('fasta'))
 
 
 def _exclude(args):
-    complement_ids = []
-    for line in args.ids:
-        if len(line.strip()) > 0:
-            complement_ids.append(line.strip())
-    for record in SeqIO.parse(args.fasta, 'fasta'):
-        if record.id not in complement_ids:
-            print(record.format('fasta'))
+    with open(args.ids, "r") as in_ids:
+        exclude_ids = in_ids.read().splitlines()
+    with open(args.fasta, "r") as in_fasta:
+        for record in SeqIO.parse(in_fasta, 'fasta'):
+            if record.id not in exclude_ids:
+                print(record.format('fasta'))
 
 
 def _id_list(args):
-    for record in SeqIO.parse(args.fasta, 'fasta'):
-        print(record.id)
+    with open(args.fasta, "r") as in_fasta:
+        for record in SeqIO.parse(in_fasta, 'fasta'):
+            print(record.id)
 
 
 if __name__ == '__main__':
@@ -45,29 +45,29 @@ if __name__ == '__main__':
     parser_translate = subparser.add_parser(
         'translate', help=("translate FASTA file"))
     parser_translate.add_argument(
-        'fasta', type=argparse.FileType('r'), help=("FASTA file"))
+        'fasta', help=("FASTA file"))
     parser_translate.set_defaults(func=_translate)
 
     parser_extract = subparser.add_parser(
         'extract', help=("extract sequence by ids"))
     parser_extract.add_argument(
-        'fasta', type=argparse.FileType('r'), help=("FASTA file"))
+        'fasta', help=("FASTA file"))
     parser_extract.add_argument(
-        'ids', type=argparse.FileType('r'), help=("one id in each line"))
+        'ids', help=("one id in each line"))
     parser_extract.set_defaults(func=_extract)
 
     parser_exclude = subparser.add_parser(
         'exclude', help=("exclude sequence by ids"))
     parser_exclude.add_argument(
-        'fasta', type=argparse.FileType('r'), help=("FASTA file"))
+        'fasta', help=("FASTA file"))
     parser_exclude.add_argument(
-        'ids', type=argparse.FileType('r'), help=("one id in each line"))
+        'ids', help=("ids to remove (txt)"))
     parser_exclude.set_defaults(func=_exclude)
 
     parser_id_list = subparser.add_parser(
         'id_list', help=("list sequence ids"))
     parser_id_list.add_argument(
-        'fasta', type=argparse.FileType('r'), help=("FASTA file"))
+        'fasta', help=("FASTA file"))
     parser_id_list.set_defaults(func=_id_list)
 
     # Parse arguments and run the sub-command
