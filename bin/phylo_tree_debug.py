332f5ac383d351a3c5757be729efab66d5934160
shjenkins94 <shjenkins94@gmail.com>
2019-08-21 15:05:52 +0900

    add input file

--- bin/phylo_tree_debug.py
+++ bin/phylo_tree_debug.py
@@ -4,15 +4,15 @@ import argparse
 
 from Bio import Phylo
 
-__version__ = 1.0
+__version__ = 2.0
 
 INFO = """
-phylo_tree - excludes non-OR genes on the basis of a phylogenetic tree.
-When a given sequence is located in the outgroup clade, it should be discarded.
+phylo_tree - has 2 modes depending on if optional argument classi_id is
+present.
 version: %s
 
-Zuoyi Jian (jianzuoyi@gmail.com)
-lab 518, Conservation Biology on Endangered Wildlife, SiChuan University
+Original program by Zuoyi Jian
+Modified by Scott Jenkins
 """ % __version__
 
 
@@ -28,28 +28,36 @@ def phylo_tree(args):
 
 # 3. Add clades that contain outgroups to outgroup.
     for non_or_id in non_or:
-        outgroup.append(
-            next(tree.find_clades({"name": non_or_id})))
+        outgroup.append(next(tree.find_clades(non_or_id)))
 
-# 4. Potential ors that aren't in the outgroup clades are added to orgroup.
-    terminals = tree.get_terminals()
-    for terminal in terminals:
-        if terminal not in outgroup:
-            orgroup.append(terminal)
+    # 4. The program diverges based on whether the classi_id parameter was
+    #    supplied.
+    if not args.classi_id:
+        # 6.a if outgroup is not monophyletic, then a non or is in the mrca
+        #   clade. once thats discarded, will others be revealed?
+        if not tree.is_monophyletic(outgroup):
+            omrca = tree.common_ancestor(non_or)
+            for terminal in omrca.get_terminals():
+                if terminal not in orgroup:
+                    print(terminal.name)
+    else:
+        # 5.b Convert class1_id to a list
+        with open(args.classi_id, "r") as in_classi_id:
+            c1 = in_classi_id.read().splitlines()
 
-    # If genes with that in the outgroup share one MRCA, it may be a non-OR
-    # gene and will be discarded.
+        # 6.b Add clades with class I ORs to classI
+        classI = []
 
+        for c1_id in c1:
+            classI.append(next(tree.find_clades(c1_id)))
 
-#   if outgroup is not monophyletic, then a non or is in the mrca clade.
-#   once thats discarded, will others be revealed?
-# 5. If the outgroup clades are not monophyletic
-    if not tree.is_monophyletic(outgroup):
-        mrca = tree.common_ancestor(non_or)
-        terminals = mrca.get_terminals()
-        for terminal in terminals:
-            if terminal in orgroup:
-                print(terminal.name)
+        tree.root_with_outgroup(outgroup)
+
+        if not tree.is_monophyletic(classI):
+            cImrca = tree.common_ancestor(classI)
+            for terminal in cImrca.get_terminals():
+                if terminal not in classI:
+                    print(terminal.name)
 
 
 # 1. Take in arguments nwk and outgroup_id, and send them to phylo_tree
@@ -57,6 +65,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=INFO)
     parser.add_argument("nwk", help=("phylogenetic tree (nwk)"))
     parser.add_argument("outgroup_id", help=("ids of outgroup (txt)"))
-
+    parser.add_argument("-C", "--classi_id", help=("ids of class I (txt)"))
     args = parser.parse_args()
     phylo_tree(args)
